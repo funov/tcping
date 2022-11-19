7c25b2691e7026a09218ee4f5cd3e750c65a14e0
Ivan Sychev <funovichf@gmail.com>
2022-11-20 04:40:44 +0500

    add basic statistics

--- ping_process.py
+++ ping_process.py
@@ -17,36 +17,42 @@ def run(
 
     if count is None:
         seq = 1
+        times = []
         while True:
             try:
                 icmp_reply, timer = ping(destination, port, wait)
 
-                print_ping(
-                    destination,
-                    port,
-                    seq,
-                    icmp_reply.ttl,
-                    timer.get_ms_str()
-                )
+                if len(icmp_reply.bytes_reply) != 0:
+                    times.append(timer.get_ms())
+
+                    print_ping(
+                        destination,
+                        port,
+                        seq,
+                        icmp_reply.ttl,
+                        timer.get_ms_str()
+                    )
+                else:
+                    print('WRONG')
 
                 if interval is not None:
                     sleep(interval)
 
                 seq += 1
             except KeyboardInterrupt:
-                print_statistics()
+                print_statistics(destination, times)
                 break
     else:
-        pass
+        for i in range(count):
+            print(i)
 
 
 def ping(destination: str, port: int, wait: float) -> (ICMPEchoReply, Timer):
     timer = Timer()
     timer.start()
     socket = ICMPSocket(wait)
-    icmp_reply = ICMPEchoReply(
-        socket.send_to(ICMPEchoRequest(), destination, port)
-    )
+    reply = socket.send_to(ICMPEchoRequest(), destination, port)
+    icmp_reply = ICMPEchoReply(reply)
     timer.stop()
     socket.close()
 
@@ -61,5 +67,9 @@ def print_ping(host: str, port: int, seq: int, ttl: int, ms: str):
     print(f'Connected to {host}[:{port}]: seq={seq} ttl={ttl} time={ms}')
 
 
-def print_statistics():
-    print(f'Statistics')
+def print_statistics(host: str, times: list):
+    n = len(times)
+
+    print(f'\n--- {host} ping statistics---')
+    print(f'{n} packets transmitted, time {sum(times)} ms')
+    print(f'rtt min/avg/max = {min(times)}/{sum(times) / n}/{max(times)} ms')
