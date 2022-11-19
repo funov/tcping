fa9ecc82cd92779993b6b85a831a72e68f3e21d8
Ivan Sychev <funovichf@gmail.com>
2022-11-20 04:13:24 +0500

    add keys and no count usage

--- network/icmp_echo_reply.py
+++ network/icmp_echo_reply.py
@@ -1,3 +1,7 @@
+import struct
+
+
 class ICMPEchoReply:
     def __init__(self, reply: bytes):
         self.bytes_reply = reply
+        self.ttl = int(struct.unpack("B", reply[0][8:9])[0])
--- ping_process.py
+++ ping_process.py
@@ -1,3 +1,4 @@
+from network.icmp_echo_reply import ICMPEchoReply
 from network.icmp_socket import ICMPSocket
 from network.icmp_echo_request import ICMPEchoRequest
 from utils.timer import Timer
@@ -12,23 +13,53 @@ def run(
         interval: float,
         wait: float
 ):
+    print_start(destination)
+
     if count is None:
+        seq = 1
         while True:
             try:
-                timer = Timer()
+                icmp_reply, timer = ping(destination, port, wait)
 
-                timer.start()
-
-                socket = ICMPSocket(wait)
-                socket.send_to(ICMPEchoRequest(), destination, port)
-
-                timer.stop()
-
-                print(timer.get_ms_str())
+                print_ping(
+                    destination,
+                    port,
+                    seq,
+                    icmp_reply.ttl,
+                    timer.get_ms_str()
+                )
 
                 if interval is not None:
                     sleep(interval)
+
+                seq += 1
             except KeyboardInterrupt:
+                print_statistics()
                 break
     else:
         pass
+
+
+def ping(destination: str, port: int, wait: float) -> (ICMPEchoReply, Timer):
+    timer = Timer()
+    timer.start()
+    socket = ICMPSocket(wait)
+    icmp_reply = ICMPEchoReply(
+        socket.send_to(ICMPEchoRequest(), destination, port)
+    )
+    timer.stop()
+    socket.close()
+
+    return icmp_reply, timer
+
+
+def print_start(host: str):
+    print(f'PING {host} 48 bytes of data')
+
+
+def print_ping(host: str, port: int, seq: int, ttl: int, ms: str):
+    print(f'Connected to {host}[:{port}]: seq={seq} ttl={ttl} time={ms}')
+
+
+def print_statistics():
+    print(f'Statistics')
